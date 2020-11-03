677bb552a3ceeb6f016748444e511ec3538aeda0
becca <rtviner@gmail.com>
2020-11-02 18:33:31 -0700

    uncommented fetch requests and tweaked array displaying articles

--- client/components/top3list.jsx
+++ client/components/top3list.jsx
@@ -1,34 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import Article from './article.jsx';
 
-function Top3list(){
+function Top3list() {
   const [top3Articles, setTop3Articles] = useState([]);
 
   // fetch request that fetches top three articles
   useEffect(() => {
-    fetch('/api/top3list')
+    fetch('/api/links/top3list')
+    .then(res => res.json())
+    .then(data => {
+      setTop3Articles(data);
+    })
+    .catch(err => console.log(err));
   });
-  
-  //   .then(res => res.json())
-  //   .then(data => {
-  //     setTop3Articles(data);
-  //   })
-  //   .catch(err => console.log(err));
-  // const articles = [];
-  // data.map(article => {
-  //   articles.push(<Article />)
-  // })
-  // const articles = [];
-  // top3Articles.map((article) => {
-  //   articles.push(<Article url="www.newyorktimes.com" title="New York Times" />);
-  // });
+    
+  const articles = top3Articles.map(article => {
+      return <Article url={article.url} title={article.title} />;
+  })
   
   return (
     <div id="top-list">
-      {/* {articles} */}
       <div id="top-articles-container">
-      <Article url="http://www.newyorktimes.com" title="New York Times" />
-      <Article url="http://www.google.com" title="Google" />
+      {articles}
       </div>
     </div>
   )
